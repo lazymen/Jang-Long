1b92f62640f050ca59687c44640066a1a6cc420b
lazymen <yjh625357@gmail.com>
2026-07-11 23:37:28 +0900

    Update homepage slider

---
+++ script.js
@@ -0,0 +1,44 @@
+const slides = document.querySelectorAll(".slide");
+
+const next = document.querySelector(".next");
+const prev = document.querySelector(".prev");
+
+let current = 0;
+
+function showSlide(index){
+
+    slides.forEach(slide=>{
+        slide.classList.remove("active");
+    });
+
+    slides[index].classList.add("active");
+
+}
+
+next.addEventListener("click",()=>{
+
+    current++;
+
+    if(current >= slides.length){
+
+        current = 0;
+
+    }
+
+    showSlide(current);
+
+});
+
+prev.addEventListener("click",()=>{
+
+    current--;
+
+    if(current < 0){
+
+        current = slides.length - 1;
+
+    }
+
+    showSlide(current);
+
+});
